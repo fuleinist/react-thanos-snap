d22fd8b727b67edbcba9c590a1a687a2aca0751c
fuleinist <fuleinist@gmail.com>
2019-09-02 10:21:41 +1000

    adding

---
+++ example/default/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import { SnapButton, SnapWrapper } from '../../../src/index';
+
+function App() {
+  return (
+    <div className="App">
+      <SnapButton />
+    </div>
+  );
+}
+
+export default App;
